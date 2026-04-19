48e9aed08c33406410d995bae5598e81d0e20ea1
vicky <pratamavr@gmail.com>
2026-04-19 22:36:50 +0800

    updates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -535,7 +535,7 @@
 
     function getCurrencySymbol(code) {
     const currency = state.currencies.find(c => c.code === code);
-    alert(JSON.stringify(currency), code);
+    alert(currency + '\n' + code);
     return currency?.symbol || code;
     }
 
