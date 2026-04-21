6d861822b45a1200e6d1c2edd7cde3f60b7da9bd
vicky <pratamavr@gmail.com>
2026-04-21 17:54:51 +0800

    updates

--- app/Http/Controllers/Api/StatementController.php
+++ app/Http/Controllers/Api/StatementController.php
@@ -253,7 +253,7 @@ class StatementController extends Controller
           'wallet_id' => $wallet->id,
           'category_id' => $trx->category_id,
           'type' => $transactionType,
-          'amount' => $trx->amount,
+          'amount' => (int) $trx->amount / 100,
           'transaction_date' => $trx->transaction_date,
           'description' => $trx->description,
           'metadata' => ['imported_from_statement_id' => $statement->id],
