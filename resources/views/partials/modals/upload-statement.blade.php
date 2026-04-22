49f96ffaf3d82bcfecde96af204225da825403f7
vicky <pratamavr@gmail.com>
2026-04-23 02:19:45 +0800

    updates

--- resources/views/partials/modals/upload-statement.blade.php
+++ resources/views/partials/modals/upload-statement.blade.php
@@ -297,7 +297,7 @@
     document.getElementById('upload-progress')?.classList.remove('d-none');
     const submitBtn = form.closest('.modal').querySelector('.btn-primary');
     submitBtn.disabled = true;
-    alert(JSON.stringify(formData));
+    alert(JSON.stringify(formData.entries()));
 
     try {
       const res = await tgApp.fetchWithAuth(BASE_URL + '/api/fintech/statements/upload', {
