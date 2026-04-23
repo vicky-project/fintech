902bce9b08a650a3276d5df469db16a61be32d17
vicky <pratamavr@gmail.com>
2026-04-24 00:31:02 +0800

    updates

--- resources/views/partials/modals/upload-statement.blade.php
+++ resources/views/partials/modals/upload-statement.blade.php
@@ -130,10 +130,10 @@
     onchange="updateSelectedCount()">
     </div>
     <div class="flex-grow-1">
-    <div class="d-flex justify-content-between align-items-start">
-    <p>
+    <div class="mb-1 d-flex align-items-start">
     <small class="text-muted">${formatDate(trx.date)}</small>
-    </p>
+    </div>
+    <div class="d-flex justify-content-between align-items-start">
     <div class="me-2" style="max-width: 70%;">
     <div class="fw-semibold text-truncate" title="${trx.description}">${trx.description}</div>
     </div>
