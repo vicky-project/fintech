f3a01f86fead19cbfc0a3bc696339e514d9c48ef
vicky <pratamavr@gmail.com>
2026-04-23 12:35:02 +0800

    updates

--- app/Http/Requests/UploadStatementRequest.php
+++ app/Http/Requests/UploadStatementRequest.php
@@ -54,7 +54,7 @@ class UploadStatementRequest extends FormRequest
     return [
       "file.required" => "File statement wajib diunggah",
       "file.file" => "Berkas tidak valid",
-      "file.mimetypes" => "File :input ({$uploadedFileName}) tidak didukung. File anda bertipe: {$uploadedFileExt}. Format yang diizinkan: ".collect($allowedExt)->join(", ", " and "),
+      "file.mimetypes" => "File :input ({$uploadedFileName}) tidak didukung. File anda bertipe: ({$uploadedFileExt}). Format yang diizinkan: ".collect($allowedExt)->join(", ", " and "),
       "file.max" => "Ukuran file maximal 10Mb"
     ];
   }
