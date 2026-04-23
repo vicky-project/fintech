e578b4c6dbab665101e4ec4191bae37a6b179116
vicky <pratamavr@gmail.com>
2026-04-23 22:53:37 +0800

    updates

--- app/Services/Decryptors/ExcelDecryptor.php
+++ app/Services/Decryptors/ExcelDecryptor.php
@@ -20,7 +20,7 @@ class ExcelDecryptor
     } catch (ReaderException $e) {
       Log::debug("Reader Exception: " . $e->getMessage(), ["path" => $filePath]);
       $message = strtolower($e->getMessage());
-      return str_contains($message, 'password') || str_contains($message, 'encrypted') || str_contains($message, 'protected') || str_contains($message, 'Unable to identify');
+      return str_contains($message, 'password') || str_contains($message, 'encrypted') || str_contains($message, 'protected') || str_contains($message, 'unable to identify');
     } catch (\Exception $e) {
       $message = strtolower($e->getMessage());
       Log::debug("Exception: ". $message);
