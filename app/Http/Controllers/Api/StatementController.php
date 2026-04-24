1bf9c2e8e79b36f7b23b5a1653acef3d2e53f04b
vicky <pratamavr@gmail.com>
2026-04-24 20:58:03 +0800

    updates

--- app/Http/Controllers/Api/StatementController.php
+++ app/Http/Controllers/Api/StatementController.php
@@ -49,6 +49,7 @@ class StatementController extends Controller
   {
     try {
       $validated = $request->validated();
+      \Log::debug("Validated data", ["data" => $validated]);
       $result = $this->statementService->uploadStatement(
         $request->user()->id,
         $validated['file'],
