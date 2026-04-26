12f40ff4db4b4cb05b5337f053a642275c84dc73
vicky <pratamavr@gmail.com>
2026-04-26 13:57:24 +0800

    updates

--- routes/api.php
+++ routes/api.php
@@ -95,7 +95,7 @@ Route::middleware(['auth:sanctum', 'pin.session'])->prefix('fintech')->name('fin
 
 
 Route::middleware(['auth:sanctum'])->prefix('fintech')->name('fintech.')->group(function() {
-  Route::get('search', [SearchController::class, 'global']):
+  Route::get('search', [SearchController::class, 'global']);
   Route::get('settings', [SettingController::class, 'show'])->middleware('pin.session');
   Route::put('settings', [SettingController::class, 'update']);
   Route::post('settings/verify-pin', [SettingController::class, 'verifyPin'])->middleware('throttle:10,1');
