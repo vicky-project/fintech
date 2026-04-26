342d08d4872ccf87a14746fb9cacdb3354d4b8cb
vicky <pratamavr@gmail.com>
2026-04-27 02:30:26 +0800

    updates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -258,6 +258,10 @@
 @endsection
 
 @push('scripts')
+<script src="//cdn.jsdelivr.net/npm/eruda"></script>
+<script>
+  eruda.init(); // Ikon Eruda akan muncul
+</script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.min.js"></script>
 <script>
   const BASE_URL = '{{ rtrim(config("app.url"), "/") }}';
