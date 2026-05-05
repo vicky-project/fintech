8668d9de15facb6365a4ba33707dd9be5f4c2612
vicky <pratamavr@gmail.com>
2026-05-06 01:33:00 +0800

    updates

--- database/migrations/2026_05_06_053812_create_user_category_rules_table.php
+++ database/migrations/2026_05_06_053812_create_user_category_rules_table.php
@@ -26,6 +26,6 @@ return new class extends Migration
 
     public function down(): void
     {
-      Schema::dropIfExists('user_category_rules');
+      Schema::dropIfExists('fintech_user_category_rules');
     }
   };
